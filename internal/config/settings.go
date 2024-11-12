77bdf3925ea3f34d87976502651b2966c1ee2688
Igor Viskov <i.viskov@samolet.ru>
2024-11-12 20:56:56 +0300

    fix

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -1,6 +1,6 @@
 package config
 
 var (
-	AuthSecretKey  = "fdVh76iKh7dsfG!7^uPhj"
+	AuthSecretKey  = "fdVh76iKh7dsfG!7^uPhj" // Это попросту упрощение, берите ключ из секретов
 	AuthCookieName = "auth"
 )
